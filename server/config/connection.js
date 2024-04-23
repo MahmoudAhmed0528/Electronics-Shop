439d4d9abd44f87e7b9d8b6d98d49a216abb402b
MahmoudAhmed <mahmood.adel@hotmail.com>
2024-04-23 17:10:15 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/electronics-shop"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Electronics-shop"
 );
 
 module.exports = mongoose.connection;
